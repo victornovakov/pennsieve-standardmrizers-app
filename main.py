553c75a529cd546f4bdc16ccb30739913f80a109
Edmore Moyo <etmoyo@gmail.com>
2024-06-18 11:53:52 -0400

    initial commit

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ print("start of processing")
 src = os.environ['INPUT_DIR']
 dest = os.environ['OUTPUT_DIR']
 
+print("Command line arguments ...")
+print(sys.argv)
+print("ENV variables ...")
+print(os.environ)
+
 shutil.copytree(src, dest, dirs_exist_ok=True)
 print("end of processing")
